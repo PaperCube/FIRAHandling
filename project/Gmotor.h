56e2c281ceeede2e2c5ed2bf0cb04bda7f969848
VisualDust <VisualDust@outlook.com>
2019-07-08 21:42:14 +0800

    emmmm

--- project/Gmotor.h
+++ project/Gmotor.h
@@ -1,44 +1,49 @@
 #define abs(a) a > 0 ? a : -a
 
-class Gmotor
-{
-private:
-    int cnt1, cnt2;
-    int speed;
-    int solid;
-    double prop = 1.0;
-    int direc = 0;
+class Gmotor {
+  private:
+    int    cnt1, cnt2;
+    int    speed;
+    int    solid         = 0;
+    double prop          = 1.0;
+    int    direc         = 0;
+    int    negativeSpeed = 200, negativeRuntime = 100;
 
-public:
-    Gmotor(int, int); // initial your motor with two pwm output pin num. check
-                      // the direction of your motor first
+  public:
+    Gmotor(int, int); // initial your motor with two pwm output pin num.
+                      // check the direction of your motor first
     void stop(int mode);
     void setSolid(int); // when you try to use stop() method, this param decides
                         // the way how your motor stop
+    void setNegativeSpeed(int);
+    void setNegativeRuntime(int);
+
     void setSpeed(int); // no more than 255 please
-    int getSpeed();
+    int  getSpeed();
     void setProp(double);
     void exchangePin(); // exchange the two controlPort
     void gradientTo(int, int);
     void walk(int, int);
 };
 
-Gmotor::Gmotor(int cnt1, int cnt2)
-{
-    this->cnt1 = cnt1;
-    this->cnt2 = cnt2;
+Gmotor::Gmotor(int cnt1, int cnt2) {
+    this->cnt1  = cnt1;
+    this->cnt2  = cnt2;
     this->speed = 0;
 }
 
-void Gmotor::stop(int mode)
-{
-    if (mode != 0)
-    {
-        int conc = this->speed / -60;
-        for (int i = 0; i <= 60; i++)
-        {
+void Gmotor::stop(int mode) {
+    if (mode == 1) {
+        this->setSpeed(
+            this->getSpeed == 0
+                ? 0
+                : (this->getSpeed() < 0 ? negativeSpeed : -negativeSpeed));
+        delay(negativeRuntime);
+    } else if (mode == 2) {
+        double conc = this->speed / -60;
+        for (int i = 0; i <= 60; i++) {
             this->speed += conc;
-            this->setSpeed(speed * prop);
+            this->setSpeed(this->speed * prop);
             delay(5);
         }
     }
@@ -48,34 +53,38 @@ void Gmotor::stop(int mode)
 
 void Gmotor::setSolid(int solid) { this->solid = solid; }
 
-void Gmotor::setSpeed(int speed)
-{
+void Gmotor::setSpeed(int speed) {
     if ((speed < 0 && this->direc == 0) || (speed > 0 && this->direc == 1))
         this->exchangePin();
-    speed = speed < 0 ? -speed : speed;
+    speed       = speed < 0 ? -speed : speed;
     this->speed = speed;
     analogWrite(cnt1, 0);
     analogWrite(cnt2, (double)(this->speed * prop));
+}
+
+void setNegativeSpeed(int negativeSpeed) {
+    this->negativeSpeed = negativeSpeed;
+}
+
+void setNegativeRuntime(int negativeRuntime) {
+    this->negativeRuntime = negativeRuntime;
 }
 
 int Gmotor::getSpeed() { return this->speed; }
 
 void Gmotor::setProp(double prop) { this->prop = prop; }
 
-void Gmotor::exchangePin()
-{
-    int t = this->cnt1;
-    this->cnt1 = this->cnt2;
-    this->cnt2 = t;
+void Gmotor::exchangePin() {
+    int t       = this->cnt1;
+    this->cnt1  = this->cnt2;
+    this->cnt2  = t;
     this->direc = this->direc == 0 ? 1 : 0;
 }
 
-void Gmotor::gradientTo(int speed, int ms)
-{
-    double sub = (double)(speed - this->speed) / (ms / 5);
+void Gmotor::gradientTo(int speed, int ms) {
+    double sub          = (double)(speed - this->speed) / (ms / 5);
     double currentSpeed = this->speed;
-    for (int i = 1; i <= ms / 5; i++)
-    {
+    for (int i = 1; i <= ms / 5; i++) {
         currentSpeed += sub;
         this->setSpeed((int)currentSpeed);
         // Serial.print("gradientTo: changing speed to ");
